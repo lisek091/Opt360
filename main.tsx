8bd1d9f9bcbf536413dec40a96b86f6f47797856
Mateusz Lisowski <lisina091@gmail.com>
2022-01-30 17:23:39 +0100

    some css

--- main.tsx
+++ main.tsx
@@ -39,12 +39,12 @@ function zrobcos(data: dataType) {
                 label: 'Osób w wieku',
                 data: [ageArray[0], ageArray[1], ageArray[2], ageArray[3], ageArray[4], ageArray[5]],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(255, 159, 64, 0.5)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
